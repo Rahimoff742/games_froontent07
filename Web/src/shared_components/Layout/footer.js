ac59d83afde2be91e7df7cd66dfb595c22598039
Rahimoff742 <rahmatjonazadov@gmail.com>
2022-11-16 17:55:02 +0500

    project tugadi

---
+++ Web/src/shared_components/Layout/footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="p-4 mt-12 text- w-full gap-9 bg-green text-white flex items-center justify-between">
+      Games footer 
+    </footer>
+  );
+}
+
+export default Footer;
+
